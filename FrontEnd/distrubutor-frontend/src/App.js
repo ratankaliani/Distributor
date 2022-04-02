7f39935c16c88f348eda317bf6ca5cbd2ddfba4d
emmaguo13 <emma@proofofalpha.io>
2022-04-02 17:16:57 -0400

    frontend start and file structure remodel

--- FrontEnd/distrubutor-frontend/src/App.js
+++ FrontEnd/distrubutor-frontend/src/App.js
@@ -1,25 +1,20 @@
+import React from 'react';
+import { Router } from '@reach/router';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Router>
+      {/* <Home path="/" />
+      <LoginController path="/login" />
+      <Register path="/register" />
+      <Form path="form" />
+      <UserStatus path="user-status" />
+      <ApproveRequests path="approve-requests" />
+      <Deed path="/deed/:id" /> */}
+    </Router>
+  </div>
+);
 
 export default App;
