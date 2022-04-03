72c28fbf37d7a0fd3b62e8a9f747efbbf37672dc
Naman Kapasi <kapasinh@berkeley.edu>
2022-04-03 02:18:46 -0400

    Front End Deposit Page

--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React,{ useState} from 'react'
 import './navbar.css'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../assets/logo.png'
+import logo from '../../assets/mint.png'
 import {  Link } from "react-router-dom";
 
 const Menu = () => (
